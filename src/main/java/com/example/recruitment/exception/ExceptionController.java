87c45e66f20b9d1339e1452469e4f0b8d6a1deaa
nuni <gkje123@naver.com>
2024-06-16 12:05:20 +0900

    #세번째 커밋 수정사항: 1. ErrorCode, CustomException, ExceptionHandler 생성(두번째 커밋시 누락됨)

---
+++ src/main/java/com/example/recruitment/exception/ExceptionController.java
@@ -0,0 +1,25 @@
+package com.example.recruitment.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ExceptionResponse> exceptionHandler(CustomException c) {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(c.getMessage(), c.getErrorCode()));
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    public static class ExceptionResponse {
+        private String message;
+        private ErrorCode errorCode;
+    }
+}
